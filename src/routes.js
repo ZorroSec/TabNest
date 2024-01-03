78d2f93a58a73ebde9333f100a96bcb5a6c195e8
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 13:55:17 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import { Sequelize, DataTypes } from "sequelize";
 import Logins from "../app/logins/logins.js";
 import connection from "../app/connection/tabnest.js";
 app.get('/', (req, res)=>{
-    connection.query(`INSERT INTO ${process.env.DBNAME}.logns(nome,email,senha) VALUES('admin2','admin2@gmail.com','admin1234')`, (results, fields)=>{
+    connection.query(`INSERT INTO ${process.env.DBNAME}.logins(nome,email,senha) VALUES('admin2','admin2@gmail.com','admin1234')`, (results, fields)=>{
         res.json({
             message: "success"
         })
