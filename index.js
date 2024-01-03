7afc3ac4aaec5677b8b7853abc0da7820a743b6a
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 19:00:18 -0300

    Errors, ja estou sem um pingo de paciencia

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { engine } from "express-handlebars"
 import bodyParser from "body-parser";
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', './src/views');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('app/public'))
