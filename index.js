fd063317f9af1092c186599ff67995990b1e908d
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 15:10:10 -0300

    Update index.js(Importing engine of the express-handlebars for index.js)

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import "./src/routes.js"
 import app from "./app/controllers/config.js"
+import { engine } from "express-handlebars"
+import bodyParser from "body-parser";
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.listen(4000, ()=>{console.log({ message: 'Success' })})
