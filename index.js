329c727bf2e7ad6ae371e011974ae451ec23f3ee
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 20:12:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import "./routes.js"
 import app from "./app/controllers/config.js"
 import express from "express"
-import { engine } from "express-handlebars"
+import exbhs from "express-handlebars"
 import bodyParser from "body-parser";
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views/');
+import path from "path";
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './views/');
+app.engine('handlebars', exbhs.engine({
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
