3f3afd0f210f93c4eb3304b450115cd5c18ae312
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 13:42:34 -0300

    UPdate db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("tabnest", "admin", "IHIsJqeO", {
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
     dialect: "mysql",
-    host: "mysql-159788-0.cloudclusters.net",
-    port: 18848
+    host: process.env.DBHOST,
+    port: process.env.DBPORT
 })
 
 export default sequelize
