be5b4dbf05664f5d6b7eb131258eb760fbbe1efb
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 11:12:18 -0300

    Update routes.js(Create a loing)

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 import app from "../app/controllers/config.js";
+import sequelize from "../app/models/db.js";
+import { Sequelize, DataType } from "sequelize";
+import Logins from "../app/logins/logins.js";
 
 app.get('/', (req, res)=>{
-    res.send('testando')
+    Logins.create({
+        nome: "admin2",
+        email: "zezao@gmail.com",
+        senha: "admin1234"
+    })
 })
