9f8e260921b4bfc3a2422491ab3d1beb3deab097
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 11:26:50 -0300

    UPdate db.js

--- app/models/db.js
+++ app/models/db.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("tabnest", "admin", "IHIsJqeO", {
     dialect: "mysql",
-    host: "mysql-159788-0.cloudclusters.net"
+    host: "mysql-159788-0.cloudclusters.net",
+    port: 18848
 })
 
 export default sequelize
