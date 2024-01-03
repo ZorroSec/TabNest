5a0dace1ceec91ec5363f41a8d2ae98c39a6e7e5
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 15:15:12 -0300

    fint: using res.render to render the website home page

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,11 @@ import { Sequelize, DataTypes } from "sequelize";
 import Logins from "../app/logins/logins.js";
 import connection from "../app/connection/tabnest.js";
 app.get('/', (req, res)=>{
-    connection.query(`INSERT INTO ${process.env.DBNAME}.logins(nome,email,senha) VALUES('admin2','admin2@gmail.com','admin1234')`, (results, fields)=>{
-        res.json({
-            message: "success"
-        })
+    // connection.query(`INSERT INTO ${process.env.DBNAME}.logins(nome,email,senha) VALUES('admin2','admin2@gmail.com','admin1234')`, (results, fields)=>{
+    //     res.json({
+    //         message: "success"
+    //     })
 
-    })
+    // })
+    res.render('home')
 })
