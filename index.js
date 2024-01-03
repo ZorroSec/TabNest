f6a6bc8cf970a2158384282d480b62a1ae25a42c
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 19:10:01 -0300

    Nao aguento

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import app from "./app/controllers/config.js"
 import express from "express"
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser";
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './src/views');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static('app/public'))
-app.use(express.static('app/scripts'))
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './src/views');
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.use(express.static('app/public'))
+// app.use(express.static('app/scripts'))
 app.listen(4000, ()=>{console.log({ message: 'Success' })})
