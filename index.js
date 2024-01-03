95e9a01da2ccc1f384aa7737891c810c6741b827
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 15:11:57 -0300

    fint: use path public/

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "./src/routes.js"
 import app from "./app/controllers/config.js"
+import express from "express"
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser";
 app.engine('handlebars', engine());
@@ -7,6 +8,6 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+app.use(express.static('app/public'))
 
 app.listen(4000, ()=>{console.log({ message: 'Success' })})
