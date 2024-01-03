914f6ffda4465077cffc5588b40b322984ccb27c
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 13:32:40 -0300

    Update tabnest.js

--- app/connection/tabnest.js
+++ app/connection/tabnest.js
@@ -1,11 +1,11 @@
 import { createConnection } from "mysql2"
 
 const connection = createConnection({
-    host: "mysql-159788-0.cloudclusters.net",
-    port: 18848,
-    user: "admin",
-    password: "IHIsJqeO",
-    database: "tabnest"
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
 })
 
 export default connection
