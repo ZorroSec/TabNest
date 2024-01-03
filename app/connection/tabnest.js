e07801374ed1500969085eb1000d226a9673a8a9
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 19:29:25 -0300

    Update tabnest.js

--- app/connection/tabnest.js
+++ app/connection/tabnest.js
@@ -1,11 +1,16 @@
 import { createConnection } from "mysql2"
 
 const connection = createConnection({
-    host: process.env.DBHOST,
-    port: process.env.DBPORT,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DBNAME
+    // host: process.env.DBHOST,
+    // port: process.env.DBPORT,
+    // user: process.env.DBUSER,
+    // password: process.env.DBPASS,
+    // database: process.env.DBNAME
+    host: "bl4brdgk6kjyan7p7h6z-mysql.services.clever-cloud.com",
+    port: 3306,
+    user: "umtkwpe4bhlrpcaj",
+    password: "WEU165EWoSsqquQeKRbc",
+    database: "bl4brdgk6kjyan7p7h6z"
 })
 
 export default connection
