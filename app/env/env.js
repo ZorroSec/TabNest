b36b60ebc5ca9e3194019716ca4ae88651ba4b4e
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 21:38:53 -0300

    Create env.js

---
+++ app/env/env.js
@@ -0,0 +1,11 @@
+const DBNAME = process.env.DBNAME || "bl4brdgk6kjyan7p7h6z"
+const DBUSER = process.env.DBUSER || "umtkwpe4bhlrpcaj"
+const DBHOST = process.env.DBHOST || "bl4brdgk6kjyan7p7h6z-mysql.services.clever-cloud.com"
+const DBPASS = process.env.DBPASS || "WEU165EWoSsqquQeKRbc"
+
+module.exports = {
+    DBNAME,
+    DBUSER,
+    DBHOST,
+    DBPASS
+}
