53beba70ce398e69e91d2260015cd34ae13b8ae7
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 21:36:31 -0300

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require("sequelize")
+const DBNAME = process.env.DBNAME || "bl4brdgk6kjyan7p7h6z"
+const DBUSER = process.env.DBUSER || "umtkwpe4bhlrpcaj"
+const DBHOST = process.env.DBHOST || "bl4brdgk6kjyan7p7h6z-mysql.services.clever-cloud.com"
+const DBPASS = process.env.DBPASS || "WEU165EWoSsqquQeKRbc"
 
-const sequelize = new Sequelize("bl4brdgk6kjyan7p7h6z", "umtkwpe4bhlrpcaj", "WEU165EWoSsqquQeKRbc", {
+const sequelize = new Sequelize(DBNAME, DBUSER, DBPASS, {
     dialect: "mysql",
-    host: "bl4brdgk6kjyan7p7h6z-mysql.services.clever-cloud.com",
+    host: DBHOST,
     port: 3306
 })
 
