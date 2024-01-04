36b8610e58c232b21fec430573db7193aabdeb1a
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-03 21:29:36 -0300

    Update routes.js(Updating the configurantions in db.js)

--- app/models/db.js
+++ app/models/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
+const sequelize = new Sequelize("bl4brdgk6kjyan7p7h6z", "umtkwpe4bhlrpcaj", "WEU165EWoSsqquQeKRbc", {
     dialect: "mysql",
-    host: process.env.DBHOST,
-    port: process.env.DBPORT
+    host: "bl4brdgk6kjyan7p7h6z-mysql.services.clever-cloud.com",
+    port: 3306
 })
 
 module.exports = sequelize
