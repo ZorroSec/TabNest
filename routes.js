b7a7dc06dade25ba998a8331854a3f3ac584cbc7
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-04 13:34:32 -0300

    UPdate route /add

--- routes.js
+++ routes.js
@@ -34,9 +34,11 @@ app.get('/tests', (req, res)=>{
     // })
 })
 app.get('/add', (req, res)=>{
-    connection.query(`INSERT INTO railway.posts(nome, titulo, post, data) VALUES('José Cipriano', 'Lorem ipsum', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', ''${Date()})`, (results, fields)=>{
+    const datetime = new Date().getTime()
+    connection.query(`INSERT INTO railway.posts(nome,titulo,post,data) VALUES('Jose Cipriano','Lorem ipsum','Hello World','${datetime}')`, (results, fields)=>{
         res.json({
-            status: 200
+            message: "success",
+            fields: fields
         })
     })
 })
